4e75132278b835fd2ad0f9fcb8cd15956179af44
JOAQUIN MURCIA ESCANEZ <jme418@inlumine.ual.es>
2023-06-04 14:11:36 +0200

    Corrección para la optimalidad del código

--- src/ejercicio2/Ejercicio2.java
+++ src/ejercicio2/Ejercicio2.java
@@ -8,25 +8,10 @@ public class Ejercicio2 {
     public List<String> listaEstudiantesNoPresentados(List<String> todos, List<String> presentados) {
         List<String> noPresentados = new ArrayList<>();
 
-        int i = 0;
-        int j = 0;
-
-        while (i < todos.size() && j < presentados.size()) {
-            String estudianteTodos = todos.get(i);
-            String estudiantePresentados = presentados.get(j);
-
-            if (estudianteTodos.equals(estudiantePresentados)) {
-                i++;
-                j++;
-            } else {
-                noPresentados.add(estudianteTodos);
-                i++;
+        for (String estudiante : todos) {
+            if (!presentados.contains(estudiante)) {
+                noPresentados.add(estudiante);
             }
-        }
-
-        while (i < todos.size()) {
-            noPresentados.add(todos.get(i));
-            i++;
         }
 
         return noPresentados;
